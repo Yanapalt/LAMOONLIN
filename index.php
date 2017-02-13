69cea4c03d7cfc52d578ccce789232cc4f927bab
weaphingyanapalt <waephing_y@silpakorn.edu>
2017-02-13 19:10:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> LA MOON LIN </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/control.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" href="css/owl.carousell.css" type="text/css" media="all"/>
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+	<div class="wrap">
+		<div class="header">
+			<div class=" nav">
+				<span class="menu"><img src="image/menu.png" alt=""> </span>	
+					<ul>
+						<li><a href="#"><i> </i>News<span>1</span></a></li>
+						<li><a href="#" ><i class="shop"> </i> Favorite</a></li>	
+						<li><a href="#" ><i class="my"> </i> Add Cart</a></li>
+						<li><a href="#" ><i class="profile"> </i>Profile</a></li>								
+					</ul>
+					<script>
+						$("span.menu").click(function(){
+							$(".nav ul").slideToggle(500, function(){
+							});
+						});
+					</script>
+			</div>
+			<div class="header-top">
+			<div class=" top-menu">
+					<span class="menu1">Menu </span>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="product.html">Menu</a></li>
+						<li><a href="#">about</a></li>
+						<li><a href="#">services</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+					<script>
+						$("span.menu1").click(function(){
+							$(".top-menu ul").slideToggle(500, function(){
+							});
+						});
+					</script>
+					
+			</div>
+			<div class=" search">
+						<form>
+							<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '';}" >
+							<input type="submit" value="" >
+						</form>
+			</div>	
+					<div class="clear"> </div>
+			
+			</div>
+			<div class="clear"> </div>
+		</div>
+	</div> <!-- div -->
+
+
+	<div class="banner">
+		<div class="left-banner">
+			<img src="image/banLogo.png">
+		</div>
+		<div class="right-banner">
+			<div class="text-banner">
+				<h1 class="animated rubberBand">LA MOON LIN</h1>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipig elit. Praesent vestibulummolestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fuscesuscipit varius mi. Cum sociis natoque penatibus et magnis.</p>
+			</div>
+			<div class="box-button-banner">
+				<button type="button" class="button-banner"><a href="product.html"><p>Order Online</p></a></button>
+				<!-- <div class="button-banner">
+					
+				</div> -->
+			</div>
+		</div>
+	</div>
+	<div class="about-site">
+		<div class="about-text">
+			<br>
+			<h2>RECEIVE GOOD FOOD</h2>
+			<h1>AND HIGH QUALITY SERVICE</h1>
+			<p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit an</p>
+		</div>
+	</div>
+	<div class="promotion">
+		<div class="promotion-left">
+			<div class="box-promotion-left">
+				<div class="promotion-left-img">
+					<div class="text-promo-left">
+						<h4>Get
+							<span>
+								20%
+							<br>
+								CASHBACK
+							</span>
+						</h4>
+						<h5>Ordered in website only</h5>
+						<a href="product.html"><h6>Order Now<div class="fa fa-arrow-circle-right"></div></h6></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="promotion-right">
+			<div class="box-promotion-right">
+				<div class="promotion-right-img">
+					<div class="text-promo-right">
+						<h4>GET Upto
+							<span>
+								
+							<br>
+								40% OFFER
+							</span>
+						</h4>
+						<h5>Monday special discount</h5>
+						<a href="product.html"><h6>Order Now<div class="fa fa-arrow-circle-right"></div></h6></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="pay">
+		<h1>How To Order Online Food</h1>
+		<p>Get your favourite food in 4 simple steps.</p>
+		<div class="order-agileinfo">  
+					<div class="col-md-3 col-sm-3 col-xs-6 order-w3lsgrids"> 
+						<div class="order-w3text"> 
+							<i class="fa fa-map" aria-hidden="true"></i> 
+							<h5>Check Order</h5>
+							<span>1</span>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6 order-w3lsgrids"> 
+						<div class="order-w3text"> 
+							<i class="fa fa-cutlery" aria-hidden="true"></i> 
+							<h5>Choose Order</h5>
+							<span>2</span>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6 order-w3lsgrids"> 
+						<div class="order-w3text"> 
+							<i class="fa fa-credit-card" aria-hidden="true"></i>
+							<h5>Pay Money</h5>
+							<span>3</span>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6 order-w3lsgrids"> 
+						<div class="order-w3text"> 
+							<i class="fa fa-truck" aria-hidden="true"></i>
+							<h5>Enjoy Food</h5>
+							<span>4</span>
+						</div>
+					</div>
+					<div class="clearfix"> </div> 
+		</div>
+	</div>
+	<div class="specail-food">
+		<div class="specail-food-left">
+			<div class="specail-food-box">
+				<div class="specail-food-box-text">
+					<h1>Special Foods</h1>
+				</div>
+			</div>
+		</div>
+		<div class="specail-food-right">
+			<div class="menu-food">
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+					<div id="captioned-gallery">
+						<figure class="slider">
+							<figure>
+								<img src="image/food1.png" alt>
+								<figcaption>Kluay khek</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food2.png" alt>
+								<figcaption>Quail candy eggs</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food3.png" alt>
+								<figcaption>Antique coffee</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food4.png" alt>
+								<figcaption>Milk tea</figcaption>
+							</figure>
+							<figure>
+								<img src="iamge/food5.png" alt>
+								<figcaption>Green tea</figcaption>
+							</figure>
+						</figure>
+					</div>
+					<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+			</div>
+			<div class="menu-food">
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+					<div id="captioned-gallery">
+						<figure class="slider">
+							<figure>
+								<img src="image/food2.png" alt>
+								<figcaption>Quail candy eggs</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food3.png" alt>
+								<figcaption>Antique coffee</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food4.png" alt>
+								<figcaption>Milk tea</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food5.png" alt>
+								<figcaption>Green tea</figcaption>
+							</figure>
+							<figure>
+								<img src="iamge/food1.png" alt>
+								<figcaption>Kluay khek</figcaption>
+							</figure>
+						</figure>
+					</div>
+					<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+			</div>
+			<div class="menu-food">
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+					<div id="captioned-gallery">
+						<figure class="slider">
+							<figure>
+								<img src="image/food3.png" alt>
+								<figcaption>Antique coffee</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food4.png" alt>
+								<figcaption>Milk tea</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food5.png" alt>
+								<figcaption>Green tea</figcaption>
+							</figure>
+							<figure>
+								<img src="image/food1.png" alt>
+								<figcaption>Kluay khek</figcaption>
+							</figure>
+							<figure>
+								<img src="iamge/food2.png" alt>
+								<figcaption>Quail candy eggs</figcaption>
+							</figure>
+						</figure>
+					</div>
+					<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+			</div>
+		</div>
+			
+	</div>		
+	<div class="content-footer">
+		<div class="content-footer-left">
+			<div class="content-footer-left-text">
+				<h2>SIGN UP FOR OUR
+				<br>
+					EMAIL UPDATES
+				</h2>
+				<h5>Stay up to date with our whitepapers, articles,
+				<br>
+					webbinars and invitations to spacial events.
+				</h5>
+			</div>
+			<div class="content-footer-left-email">
+				<div class="input-group">
+					<input type="text" class="form-control" aria-label="..." placeholder="Email address...">
+		  			<div class="input-group-btn">
+		   			 <button type="button" class="btn btn-default" aria-label="Help">
+		   			 	<span class="glyphicon glyphicon-question-sign"></span>
+		   			 </button>
+		   			 <button type="button" class="btn btn-default">Submit</button>
+		  			</div>
+				</div>
+			</div>
+		</div>
+		<div class="content-footer-left-mid">
+			<div class="content-footer-mid-text">
+				<h2>STAY CONNECTED
+				<br>
+					WITH LISTRAK
+				</h2>
+			</div>
+			<div class="content-footer-mid-icon">
+				<a href="index.php"><img src="image/i1.png"></a>
+				<a href="index.php"><img src="image/i2.png"></a>
+				<a href="index.php"><img src="image/i3.png"></a>
+				<a href="index.php"><img src="image/i4.png"></a>
+				<br>
+				<a href="index.php"><img src="image/i5.png"></a>
+				<a href="index.php"><img src="image/i6.png"></a>
+				<a href="index.php"><img src="image/i7.png"></a>
+			</div>
+		</div>
+		<div class="content-footer-left-right">
+			<div class="content-footer-right-text">
+				<h2>AFFILIATIONS AND
+				<br>
+					ACKNOWLEDGEMENTS
+				</h2>
+			</div>
+
+		</div>
+			
+	</div>
+	<div class="line-footer">
+		<div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+			    <span class="sr-only">100% Complete</span>
+			  </div>
+	</div>
+	<div class="under-footer">
+		<h6 class="glyphicon glyphicon-copyright-mark"> 2017 LA MOON LIN. All rights reserved</h6>
+	</div>
+
+
+	<script src="js/bootstrap.js"></script>
+	<div id='easy-top'/>
+	  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+	<script src='https://cdn.rawgit.com/Arlina-Design/redvision/master/easing.js'></script>
+
+	    <script src="js/index.js"></script>
+</body>
+</html>
